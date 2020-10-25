460893ba5de1cefa1fb33f8c428aba6403b4cce4
unknown <3nriqu3.or@gmail.com>
2020-10-25 17:13:27 +0100

    Prueba delete

--- Actividad 1 Menu Procesos/src/Package/Main.java
+++
@@ -1,9 +0,0 @@
-package Package;
-
-public class Main {
-    public static void main(String Args[]){
-        if (System.getProperty("os.name").toLowerCase().contains("windows")){
-            Windows.ejecutarProcesos();
-        } else { Linux.ejecutarProcesos();}
-    }
-}
